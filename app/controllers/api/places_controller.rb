0575ef69a80a5c12fd889d8f1ec76a9ad87e2d1e
benmfoster <bfoster@marlboro.edu>
2019-06-18 16:13:40 -0400

    first commit

---
+++ app/controllers/api/places_controller.rb
@@ -0,0 +1,40 @@
+class Api::PlacesController < ApplicationController
+    def index
+        @places = Place.all
+        render 'index.json.jbuilder'
+    end
+
+    def create
+        @place = Place.new(
+            name: params[:name],
+            address: params[:address]
+        )
+        if @place.save
+			render 'show.json.jbuilder'
+		else
+			render json: {errors: @place.errors.full_messages}, status: 422
+		end
+    end
+
+    def show
+        @place = Place.find(params[:id])
+		render 'show.json.jbuilder'
+    end
+
+    def update
+        @place = Place.find(params[:id])
+        @place.name = params[:name] || @place.name
+        @place.address = params[:address] || @place.address
+        if @place.save
+			render 'show.json.jbuilder'
+		else
+			render json: {errors: @place.errors.full_messages}, status: 422
+		end
+    end
+
+    def destroy
+        @place = Place.find(params[:id])
+		@place.destroy
+		render json: {message: "Place successfully destroyed!"}
+    end
+end
